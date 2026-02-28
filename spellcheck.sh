3e9d1257389a68cb296fec77c4f63792179db40f
anarcat <anarcat@dorothea.anarc.at>
2026-02-27 23:35:46 -0500

    translate homepage to french
    This required quite a few steps:
    
     1. i had to switch back to mkdocs. not super excited about this
        because the titles popped back in again, but it's the only way to
        get a working i18n plugin
    
     2. i had to ignore french files for typos, which doesn't support
        french
    
     3. i had to figure out how personal dictionaries work in aspell,
        which is non-trivial
    
    For the latter, I essentially rely on `aspell check` to edit the
    dictionaries for me.

---
+++ spellcheck.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+lang="$1"
+
+if ! dpkg-query --show aspell aspell-"$lang" >/dev/null; then
+    apt-get update && apt-get install -y aspell aspell-"$lang"
+fi
+
+aspell="aspell --mode=markdown --lang=$lang --home-dir=. --personal=aspell.$lang.pws --encoding=utf-8"
+
+find -name "*.$lang.md" | while read path; do
+    echo "spellchecking $path..."
+    if $aspell list < "$path" | grep .; then
+        echo "found above misspelled in $path"
+        echo "run aspell with: $aspell check $path"
+        exit 1
+    fi
+done
+
+echo "all checked language $lang"
